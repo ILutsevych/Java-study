18552c2a5c0b1c8e3b87722734fe374a051f8007
ILutsevych <ira98777@gmail.com>
2019-02-18 20:56:27 +0200

    First commit

---
+++ src/com/epam/example/HomeWork1.java
@@ -0,0 +1,23 @@
+
+package com.epam.example;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+
+    public static void main (String[]arg) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("* * * *   * * * *      *        *       *");
+        System.out.println("                                         ");
+        System.out.println("*         *     *    *   *      *  *  * *");
+        System.out.println("                                         ");
+        System.out.println("* *       * * * *   *     *     *   *   *");
+        System.out.println("                                         ");
+        System.out.println("*         *        * * * * *    *       *");
+        System.out.println("                                         ");
+        System.out.println("* * * *   *      *           *  *       *");
+
+    }
+}
---
+++ src/com/epam/example/Main.java
@@ -0,0 +1,27 @@
+package com.epam.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter name ->");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name);
+
+        System.out.println(" ****       **   *   *   ** ");
+        System.out.println("      *    *  *  *   *  *  *");
+        System.out.println("      *    *  *  *   *  *  *");
+        System.out.println("      *    *  *  *   *  *  *");
+        System.out.println("      *    ****  *   *  ****");
+        System.out.println("      *    *  *   * *   *  *");
+        System.out.println("*    *     *  *   * *   *  *");
+        System.out.println(" ****      *  *   * *   *  *");
+
+
+
+
+    }
+}
