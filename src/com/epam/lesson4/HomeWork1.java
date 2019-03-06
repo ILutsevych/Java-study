953f472e9e8d2d574301c0caa470a3053ff5b8b6
ILutsevych <ira98777@gmail.com>
2019-03-06 23:01:14 +0200

    Home work 4 done. Little fix.

--- src/com/epam/lesson4/HomeWork1.java
+++ src/com/epam/lesson4/HomeWork1.java
@@ -3,15 +3,13 @@ package com.epam.lesson4;
 import java.util.Scanner;
 
 public class HomeWork1 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int a;
         String str = "";
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number --> ");
         a = sc.nextInt();
-        while(a > 0)
-        {
+        while(a > 0) {
             int y = a % 2;
             str = y + str;
             a = a / 2;
